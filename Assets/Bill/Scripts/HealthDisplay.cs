5835b88102a8d3332339ed0b5f1faeaa043c1e21
William Quintano <quintanw891@gmail.com>
2020-02-02 13:57:19 -0500

    First Working Game

---
+++ Assets/Bill/Scripts/HealthDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    public int health;
+    public Image[] images;
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0; i<images.Length; i++)
+        {
+            if (i <= health - 1)
+            {
+                images[i].sprite = fullHeart;
+            }
+            else
+            {
+                images[i].sprite = emptyHeart;
+            }
+        }
+        if (health <= 0)
+        {
+            Debug.Log("DEATH");
+            gameObject.SetActive(false);
+        }
+    }
+}
